ed640b873e14b31d4ab12cb68189b536b0dd8502
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-05-22 19:19:06 +0600

    Training component completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from chickenDieaseClassifier.pipeline.stage_01_data_ingestion import (
 from chickenDieaseClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelPipeline,
 )
+from chickenDieaseClassifier.pipeline.stage_03_training import TrainingPipeline
 
 STAGE_NAME = "Data Ingestion"
 try:
@@ -23,6 +24,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = TrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
