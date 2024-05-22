f699b1fc887141517905a6bc78442db91f9d08e9
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-05-22 20:38:19 +0600

    evaluation component completed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from chickenDieaseClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelPipeline,
 )
 from chickenDieaseClassifier.pipeline.stage_03_training import TrainingPipeline
+from chickenDieaseClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion"
 try:
@@ -37,6 +38,19 @@ try:
     model_trainer = TrainingPipeline()
     model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evalution = EvaluationPipeline()
+    model_evalution.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
     logger.exception(e)
     raise e
--- src/chickenDieaseClassifier/config/configuration.py
+++ src/chickenDieaseClassifier/config/configuration.py
@@ -6,6 +6,7 @@ from chickenDieaseClassifier.entity.config_entity import (
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
     TrainingConfig,
+    EvaluationConfig
 )
 
 
@@ -76,3 +77,13 @@ class ConfigurationManager:
             params_image_size=params.IMAGE_SIZE,
         )
         return training_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chicken-fecal-images",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE,
+        )
+        return eval_config
