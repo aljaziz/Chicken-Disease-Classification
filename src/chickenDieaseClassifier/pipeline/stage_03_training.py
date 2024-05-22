7d89e9e6dc0df903cd27e463c809171c7672c3a8
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-05-22 19:25:53 +0600

    training pipeline updated

--- src/chickenDieaseClassifier/pipeline/stage_03_training.py
+++ src/chickenDieaseClassifier/pipeline/stage_03_training.py
@@ -12,15 +12,15 @@ class TrainingPipeline:
 
     def main(self):
         config = ConfigurationManager()
-        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks_config = config.get_prepare_callbacks_config()
         prepare_callbacks = PrepareCallBacks(config=prepare_callbacks_config)
-        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+        callbacks_list = prepare_callbacks.get_tb_ckpt_callbacks()
 
         training_config = config.get_training_config()
         training = Training(config=training_config)
         training.get_base_model()
         training.train_valid_generator()
-        training.train(callback_list=callback_list)
+        training.train(callbacks_list=callbacks_list)
 
 
 if __name__ == "__main__":
