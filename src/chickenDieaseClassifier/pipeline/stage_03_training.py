752525f12b21d204447908972f8e1a9fdde89377
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-05-22 19:19:23 +0600

    Training component updated

---
+++ src/chickenDieaseClassifier/pipeline/stage_03_training.py
@@ -0,0 +1,35 @@
+from chickenDieaseClassifier.config.configuration import ConfigurationManager
+from chickenDieaseClassifier.components.prepare_callbacks import PrepareCallBacks
+from chickenDieaseClassifier.components.training import Training
+from chickenDieaseClassifier import logger
+
+STAGE_NAME = "Training"
+
+
+class TrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks = PrepareCallBacks(config=prepare_callbacks_config)
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(callback_list=callback_list)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = TrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
